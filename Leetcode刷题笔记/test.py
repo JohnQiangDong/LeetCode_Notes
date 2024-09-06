a91f35031ef6324bb54b876039f48bda6d4b5336
JohnQiangDong <32593035+JohnQiangDong@users.noreply.github.com>
2024-09-06 17:19:56 +0800

    Update test.py

--- Leetcode刷题笔记/test.py
+++ Leetcode刷题笔记/test.py
@@ -1,10 +1,6 @@
 my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York'}
 my_dict.update({'1':2})
 #print(my_dict)
-import queue
-MyQueue = queue.Queue()
-MyQueue.put(1)
-MyQueue.put(2)
-MyQueue.put(3)
-print(MyQueue.get())
-print(MyQueue.qsize())
+
+a = "3+2*3*4-1"
+print(int(a))
